85e463ff3231cf85996259c3f54a59d9f74feaa5
Drew Ku <andrewku123@gmail.com>
2017-11-15 12:05:28 -0500

    second commit

--- AI.py
+++ AI.py
@@ -1,6 +1,6 @@
-from clarifai.rest import ClarifaiApp
+from clarifai.rest.client import ClarifaiApp
 
-app = ClarifaiApp()
+app = ClarifaiApp(api_key='ef0f24a2dc7f413c9d781911db82261b')
 
 model = app.models.get('fa07ebd275514870a636b345aa3e1434')
 
